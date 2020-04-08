036a3cda04a4588de320f71fa6ffc44375834739
Moze <mosesibe@gmail.com>
2020-04-08 06:25:24 +0100

    Updates

---
+++ precache-manifest.3fb77c6b52d36f5f9a4fa52cfb3e68d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3053f8913a1f727750270e609a55ba5",
+    "url": "/mo-rolodex/index.html"
+  },
+  {
+    "revision": "91b6ed5b42646616f8d9",
+    "url": "/mo-rolodex/static/css/main.23172c60.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/mo-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mo-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b6ed5b42646616f8d9",
+    "url": "/mo-rolodex/static/js/main.07d68fa0.chunk.js"
+  },
+  {
+    "revision": "5e4de77343281129e15b",
+    "url": "/mo-rolodex/static/js/runtime-main.e8e72f84.js"
+  }
+]);
